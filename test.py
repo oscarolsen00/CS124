59b93f4fb91818dc7cf8a7abca2aef0553713ef4
Oscar Olsen <oscarolsen@dhcp-10-250-38-196.harvard.edu>
2023-02-17 15:37:05 -0500

    first1

--- test.py
+++ test.py
@@ -3,6 +3,57 @@
 # nodes have an extra dimension
 # weight using euclidian distance when dimension greater than 1 ortherwise weight is 
 
+from random import uniform as rnd
+import numpy as np
+import math as mt
+
+def euc_dist(a,b):
+    val = mt.dist(a,b)
+    return val
+
+node = 1
+
+def vertices_gen(n, dim):
+    vertices = dict()
+    if dim == 2:
+        for i in range(n):
+            vertices.update({ i : (rnd(0,1))})
+    if dim == 2:
+        for i in range(n):
+            vertices.update({ i : (rnd(0,1),rnd(0,1))})
+    if dim == 3:
+        for i in range(n):
+            vertices.update({ i : (rnd(0,1),rnd(0,1), rnd(0,1))})
+    if dim == 4:
+        for i in range(n):
+            vertices.update({ i : (rnd(0,1),rnd(0,1), rnd(0,1), rnd(0,1))})
+    return vertices
+
+
+def matrix(n, vertices):
+    graph = []
+    if dim == 1:
+        for i in range(0,n):
+            graph.append(list(np.zeros(n)))
+            for j in range(n):
+                if j > i:
+                    graph[i][j] = rnd(0,1)
+                else: 
+                    graph[i][j] = graph[j][i]
+    else:
+        for i in range(0,n ):
+            graph.append(list(np.zeros(n)))
+            for j in range(n):
+                if j > i:
+                    graph[i][j] = euc_dist()
+                else: 
+                    graph[i][j] = graph[j][i]
+
+    return graph
+            
+                #  gerneate new edge copy over otherewise
+print(matrix(3))
+
 #  matrix - becusea complete so everyone conected
 # randomly generate coordinates 
 # weights = euclidian distance of these nodes
