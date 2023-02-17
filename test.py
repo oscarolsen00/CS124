3895a2aa9eb0534f67818610fd99fbecd230543f
Oscar Olsen <oscarolsen@dhcp-10-250-38-196.harvard.edu>
2023-02-17 14:47:00 -0500

    test

---
+++ test.py
@@ -0,0 +1,11 @@
+# class to represent the graph dimensions
+
+# nodes have an extra dimension
+# weight using euclidian distance when dimension greater than 1 ortherwise weight is 
+
+#  matrix - becusea complete so everyone conected
+# randomly generate coordinates 
+# weights = euclidian distance of these nodes
+# list of node index to coordinates
+
+# an MST function
